051e42487cd34b1429340626e75eb66b3c4f4baa
Fabio Ferrarini <ferrarini09@gmail.com>
2024-11-15 07:23:55 +0100

    Update inst_docker.sh

--- install/inst_docker.sh
+++ install/inst_docker.sh
@@ -1,8 +1,17 @@
 echo "Install docker.io"
-sudo apt -y install docker.io
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo systemctl status docker
 sudo systemctl enable docker
-sudo apt -y install docker-compose
+ 
 echo "--------"
 echo "sudo usermod -aG docker ${USER}"
 echo "su - ${USER}"
