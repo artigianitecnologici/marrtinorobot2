1b35795fdf932c438162f02562d45ace31b097bf
Fabio Ferrarini <ferrarini09@gmail.com>
2024-11-27 09:41:09 +0100

    update

--- docker/pi/build/rosidl_typesupport_microxrcedds_c/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ubuntu/marrtinorobot2_ws/install/microcdr/share/microcdr/package.sh"
--- docker/pi/build/rosidl_typesupport_microxrcedds_cpp/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ubuntu/marrtinorobot2_ws/install/microcdr/share/microcdr/package.sh"
-. "/home/ubuntu/marrtinorobot2_ws/install/rosidl_typesupport_microxrcedds_c/share/rosidl_typesupport_microxrcedds_c/package.sh"
