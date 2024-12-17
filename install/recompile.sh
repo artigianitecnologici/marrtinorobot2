befa9495320543616b7f4489e85616d4eeba8d4c
Fabio Ferrarini <ferrarini09@gmail.com>
2024-12-17 21:32:11 +0100

    Update marrtinorobot2_slam

--- install/recompile.sh
+++ install/recompile.sh
@@ -48,6 +48,7 @@ ln -s $HOME/src/marrtinorobot2/marrtinorobot2_gazebo .
 ln -s $HOME/src/marrtinorobot2/marrtinorobot2_navigation .
 ln -s $HOME/src/m-explore-ros2/explore .
 #ln -s $HOME/src/OrbbecSDK_ROS2 .
+ln -s $HOME/src/marrtinorobot2/marrtinorobot2_slam .
 ln -s $HOME/src/marrtinorobot2/marrtinorobot2_vision .
 ln -s $HOME/src/marrtinorobot2/marrtinorobot2_voice .
 ln -s $HOME/src/marrtinorobot2/marrtinorobot2_cartographer .
